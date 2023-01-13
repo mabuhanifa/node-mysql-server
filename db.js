c00a5356dfed17225681bc5292c0afa461170ca0
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-14 04:53:05 +0600

    db connected

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 require("dotenv").config();
+
 const db = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: "",
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to SQL SERVER!");
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -5,4 +5,18 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = 5000;
+
+app.get("/products", (req, res) => {
+  const q = "SELECT * FROM products";
+  db.query(q, (err, data) => {
+    if (err) {
+      res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
