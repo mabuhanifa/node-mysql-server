1e341c7143a8b64612cf1f2ff413b962558e1f88
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-14 01:55:37 +0600

    server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 5000;
