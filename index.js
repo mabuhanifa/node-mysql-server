b29780affff931ea7d340fea2855ba56fda11362
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-14 10:58:12 +0600

    name updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // getting all products from the database
 app.get("/products", (req, res) => {
